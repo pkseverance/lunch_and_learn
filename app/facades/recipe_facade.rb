1367a965a2c0cb7c0087c50169815ccd96bbd8c9
Patricia Severance <patriciakseverance@gmail.com>
2023-03-05 13:34:15 -0700

    Implement recipe facade

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,3 +1,6 @@
 class RecipeFacade
-  
+  def self.search_recipes(country)
+    recipes = RecipeService.get_recipe_by_country(country)
+    recipes[:hits].map {|recipe| Recipe.new(recipe)}
+  end
 end
