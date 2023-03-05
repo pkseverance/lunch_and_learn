c21ed3c8cd90dfc35d51aba8f91060b03cf5b85e
Patricia Severance <patriciakseverance@gmail.com>
2023-03-05 13:38:41 -0700

    Add country attribute to recipe facade

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,6 +1,6 @@
 class RecipeFacade
   def self.search_recipes(country)
     recipes = RecipeService.get_recipe_by_country(country)
-    recipes[:hits].map {|recipe| Recipe.new(recipe)}
+    recipes[:hits].map {|recipe| Recipe.new(recipe, country)}
   end
 end
