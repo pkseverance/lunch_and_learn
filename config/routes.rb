1f38462bd557c085f93583b39852a164aa144cd8
Patricia Severance <patriciakseverance@gmail.com>
2023-03-05 13:33:10 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/recipes', to: 'recipes#index'
+      get '/recipes', to: 'recipes#search'
     end
   end
 end
