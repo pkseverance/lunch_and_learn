ee0ad9ab6117bcb160ed05adcddcfbec7e3611b2
Patricia Severance <patriciakseverance@gmail.com>
2023-03-06 12:05:13 -0700

    Add tests to country service to handle the get_country method

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe 'Country Service' do
       expect(result[:name][:common]).to be_a(String)
     end
   end
+
+  it 'returns a json response object containing data on a specific country' do
+    json_1 = File.read('spec/fixtures/country_service_get_country_response.json')
+
+    stub_request(:get, 'https://restcountries.com/v3.1/name/japan')
+      .to_return(status: 200, body: json_1, headers: {})
+
+    results = CountryService.get_country('japan')
+    expect(results).to be_an(Array)
+
+    result = results[0]
+    expect(result).to have_key(:name)
+    expect(result[:name]).to be_a(Hash)
+    expect(result[:name]).to have_key(:common)
+    expect(result[:name][:common]).to be_a(String)
+
+    expect(result).to have_key(:capitalInfo)
+    expect(result[:capitalInfo]).to be_a(Hash)
+
+    expect(result[:capitalInfo]).to have_key(:latlng)
+    expect(result[:capitalInfo][:latlng]).to be_an(Array)
+  end
 end
