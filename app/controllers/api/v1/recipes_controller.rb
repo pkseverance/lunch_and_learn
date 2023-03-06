f8d2ef3ae45714b2f770d7628b4543d42cab4b14
Patricia Severance <patriciakseverance@gmail.com>
2023-03-05 18:58:51 -0700

    Refactor recipes_controller to pick random country

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::RecipesController < ApplicationController
   def search
-    response = RecipeFacade.search_recipes(params[:country])
+    if (params[:country])
+      response = RecipeFacade.search_recipes(params[:country])
+    else 
+      response = RecipeFacade.search_recipes(CountryFacade.random_country_name)
+    end
+
     render json: RecipeSerializer.new(response)
   end
 end
