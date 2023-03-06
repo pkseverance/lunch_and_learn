4e09000c64f2b18218d97d6f55579da165dba258
Patricia Severance <patriciakseverance@gmail.com>
2023-03-06 13:07:52 -0700

    Implement testing for country facade

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -5,6 +5,7 @@ class CountryFacade
     Country.new(data[0])
   end
 
+  #Ignore this. It's part of the main project and I don't have tests for it yet.
   def self.random_country_name
     CountryService.get_countries.sample[:name][:common]
   end
