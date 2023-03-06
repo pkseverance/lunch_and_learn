febd7f182c5f247729e219defacb83a30e4ef888
Patricia Severance <patriciakseverance@gmail.com>
2023-03-06 11:47:42 -0700

    Implement route for tourist sights

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/recipes', to: 'recipes#search'
+      get '/tourist_sights', to: 'countries#search'
     end
   end
 end
