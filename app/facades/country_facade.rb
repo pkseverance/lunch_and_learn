6c0b8775458cc4274f6b9c2cfc1d8f9df915967e
Patricia Severance <patriciakseverance@gmail.com>
2023-03-06 12:45:18 -0700

    Implement country and place facades

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,4 +1,10 @@
 class CountryFacade
+
+  def self.country(country)
+    data = CountryService.get_country(country)
+    Country.new(data[0])
+  end
+
   def self.random_country_name
     CountryService.get_countries.sample[:name][:common]
   end
