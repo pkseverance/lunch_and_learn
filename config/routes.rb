1ad63fb6e600237746139e401a2c143e55d6742b
Patricia Severance <patriciakseverance@gmail.com>
2023-03-07 20:06:04 -0700

    Add users to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get '/recipes', to: 'recipes#search'
       get '/tourist_sights', to: 'countries#search'
       get '/learning_resources', to: 'learning_resources#search'
+
+      resources :users, only: [:create]
     end
   end
 end
