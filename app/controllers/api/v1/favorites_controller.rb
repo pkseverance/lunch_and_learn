3eebef4b844b600aa13e27c8d44ad1f4b91b4535
Patricia Severance <patriciakseverance@gmail.com>
2023-03-08 01:19:29 -0700

    Implement favorites index method

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,19 @@
 class Api::V1::FavoritesController < ApplicationController
+  def index
+    if user = User.find_by(api_key: params[:api_key])
+      favorites = user.favorites
+      render json: FavoriteSerializer.new(favorites)
+    elsif params[:api_key] == 'coffee' # This should never happen
+      render json: {
+        'failure': 'I am a teapot'
+      }, status: 418
+    else
+      render json: {
+        'failure': 'Invalid API Key'
+      }, status: 401
+    end
+  end
+
   def create
     if user = User.find_by(api_key: params[:api_key])
       favorite = user.favorites.new(favorite_params)
@@ -6,6 +21,10 @@ class Api::V1::FavoritesController < ApplicationController
       render json: {
         'success': 'Favorite added successfully'
       }, status: 201
+    elsif params[:api_key] == 'coffee' # This should never happen
+      render json: {
+        'failure': 'I am a teapot'
+      }, status: 418
     else
       render json: {
         'failure': 'Failed to find user'
@@ -14,7 +33,6 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   private
-
   def favorite_params
     params.require(:favorite).permit(:country, :recipe_link, :recipe_title)
   end
