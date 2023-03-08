02d94cf4934a183288aa4c86e8bd42b9f8b2b326
Patricia Severance <patriciakseverance@gmail.com>
2023-03-08 12:24:41 -0700

    Fixed video attribute erroring on empty data

--- app/poros/learning_resource.rb
+++ app/poros/learning_resource.rb
@@ -2,10 +2,14 @@ class LearningResource
   attr_reader :id, :type, :country, :video, :images
   def initialize(video_data, image_data, country)
     @country = country
-    @video = {
-      title: video_data[:snippet][:title],
-      youtube_video_id: video_data[:id][:videoId]
-    }
+    @video = {}
+    if video_data != nil
+      @video = {
+        title: video_data[:snippet][:title],
+        youtube_video_id: video_data[:id][:videoId]
+      }
+    end
+
     @images = image_data.map{|data| {alt_tag: data[:description], url: data[:links][:html]}}
 
   end
