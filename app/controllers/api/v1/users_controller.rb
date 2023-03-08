30427a2bece3cf2acb1f64ace911fdf16bb1c65a
Patricia Severance <patriciakseverance@gmail.com>
2023-03-07 23:32:38 -0700

    Implement controller logic for user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::UsersController < ApplicationController
-  
+  def create
+    user = User.new(user_params)
+    user.save
+    render json: UserSerializer.new(user), status: 201
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
