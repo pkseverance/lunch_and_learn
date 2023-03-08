40542cefcea9240e13faa4ed0547cffce4c9af06
Patricia Severance <patriciakseverance@gmail.com>
2023-03-07 23:32:18 -0700

    Implement generate_api_key method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  before_validation :generate_api_key
   validates_presence_of :name, :email
   validates :email, uniqueness: true
   has_many :favorites
 
-  
+  private
+  def generate_api_key
+    if self.api_key == nil
+      self.api_key = Random.hex
+    end
+  end
 end
